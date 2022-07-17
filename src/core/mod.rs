a8541ce42818f6163fb2a25e4b2f753fc2b4f48f
kivimango <balazsviktor23@gmail.com>
2022-07-17 22:23:05 +0200

    Added core module

---
+++ src/core/mod.rs
@@ -0,0 +1,10 @@
+
+/// An application event.
+/// If within that tick rate no input event happens, the application will emit a Tick.
+/// Otherwise, the input will be emitted.
+pub enum Event<I> {
+    /// User input from keyboard
+    Input(I),
+    ///
+    Tick,
+}
